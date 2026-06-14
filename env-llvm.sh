4c9f270e50de1267cf0b4d3b5117fa5e7400b976
rsuntk <90097027+rsuntk@users.noreply.github.com>
2026-06-14 13:18:17 +0700

    env-llvm: Use neutron clang

--- env-llvm.sh
+++ env-llvm.sh
@@ -1,16 +1,17 @@
 export LLVM=1
 export LLVM_IAS=1
 
-TC_DIR="$HOME/clang-22"
+TC_DIR="$HOME/neutron-clang"
 ACTIVE_COMPILER="$TC_DIR/bin/clang"
 export PATH="$TC_DIR/bin:$PATH"
 export LD_LIBRARY_PATH="$TC_DIR/lib:$TC_DIR/lib64:${LD_LIBRARY_PATH:-}"
 
 __fetch_toolchain() {
     if [ ! -d "$TC_DIR" ]; then
-        msg "Downloading AOSP-LLVM..."
+        #msg "Downloading AOSP-LLVM..."
         mkdir -p "$TC_DIR"
-        local url="https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/9b144befdfd93b90e02c663504fb9f4b95f9faf8/clang-r596125.tar.gz"
+        #local url="https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/9b144befdfd93b90e02c663504fb9f4b95f9faf8/clang-r596125.tar.gz"
+        local url="https://github.com/Neutron-Toolchains/clang-build-catalogue/releases/download/26052026/neutron-clang-26052026.tar.zst -O /tmp/neutron.tar.zst"
         curl -Ls "$url" | tar -xz -C "$TC_DIR"
     else
         msg "Toolchain already exists."
